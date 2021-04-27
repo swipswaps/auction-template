001b8918fc18dedd7923481a5d82d4cc9be9ea88
Lukas <lukas.weidich@gmx.de>
2021-04-27 21:10:45 +0200

    ⏪ enable stat fetching

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,32 +8,30 @@ import TitleAndDesc from "../components/meta/TitleAndDesc";
 import { getStats } from "../utils/api";
 import { StatsResponse } from "./api/stats";
 
-const index = () =>
-	// { stats }: { stats: StatsResponse }
-	{
-		return (
-			<>
-				<TitleAndDesc
-					title="Create attractive eBay listings with Auction Template"
-					desc="Free and easy to use tool for your eBay listings. The best editor to load your ads, and to create and download responsive descriptions with custom themes."
-				/>
-				<Hero />
-				<Features />
-				<Mission />
-				{/* <Stats stats={stats} /> */}
-				<Usage />
-			</>
-		);
-	};
+const index = ({ stats }: { stats: StatsResponse }) => {
+	return (
+		<>
+			<TitleAndDesc
+				title="Create attractive eBay listings with Auction Template"
+				desc="Free and easy to use tool for your eBay listings. The best editor to load your ads, and to create and download responsive descriptions with custom themes."
+			/>
+			<Hero />
+			<Features />
+			<Mission />
+			<Stats stats={stats} />
+			<Usage />
+		</>
+	);
+};
 
 export default index;
 
 export async function getStaticProps() {
-	// const stats: StatsResponse = await getStats();
+	const stats: StatsResponse = await getStats();
 
 	return {
 		props: {
-			// stats,
+			stats,
 		},
 		revalidate: 1,
 	};
